11021ed2cf40176407b44e8b9f39b7125e8f36d2
Gabriel Rodrigues <gabriel.rocha.andrade.rodrigues@gmail.com>
2023-03-29 16:07:28 -0300

    criando a funcão de jogar novamente e finalizando

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
+const screen1 = document.querySelector('.screen1')
+const screen2 = document.querySelector('.screen2')
 const randomNumber = Math.round(Math.random() * 10)
 
 let attempts = 1
 
-function handleClick(event) {
+function handleTryClick(event) {
   event.preventDefault()
 
   const inputNumber = document.querySelector('#inputNumber')
 
   if (Number(inputNumber.value) === randomNumber) {
-    document.querySelector('.screen1').classList.add('hide')
-    document.querySelector('.screen2').classList.remove('hide')
+    screen1.classList.add('hide')
+    screen2.classList.remove('hide')
 
     document.querySelector(
       '.screen2 h2'
@@ -18,7 +20,18 @@ function handleClick(event) {
     console.log(`Acertou em ${attempts} tentativas`)
   }
 
+  inputNumber.value = ''
   attempts++
 
   console.log(attempts)
 }
+
+const btnTry = document.querySelector('#btnTry')
+const btnReset = document.querySelector('#btnReset')
+
+btnTry.addEventListener('click', handleTryClick)
+btnReset.addEventListener('click', function () {
+  screen1.classList.remove('hide')
+  screen2.classList.add('hide')
+  attempts = 1
+})
